bbffa35ae90563f0d8e2f0cca7002064b43e9f4b
Israt-28 <62091358+Israt-28@users.noreply.github.com>
2021-06-08 23:44:28 +0600

    Add files via upload

---
+++ LAB2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WebTech Lab task 2 </title>
+</head>
+<body>
+
+<h1> WebTech Lab Task 2 </h1>
+<h3> This is a Departmental course </h3>
+<p> Name  : ISRAT JAHAN </p>
+<p> ID    : 17-34780-2</p>
+<p>My Institute:<a href="https://www.aiub.edu">AIUB</a></p>
+<p>My Institute logo:</p><img src="https://seeklogo.com/images/A/aiub-logo-1115122C7B-seeklogo.com.png" alt="AIUB logo">
+<table style="width:100%">
+  <tr>
+    <th>Department</th>
+    <th>Course Name</th>
+    <th>Grade</th>
+  </tr>
+  <tr>
+    <td>CSE</td>
+    <td>WebTech</td>
+    <td>3.70</td>
+  </tr>
+  <tr>
+    <td>BBA</td>
+    <td>B.COM</td>
+    <td>3.67</td>
+  </tr>
+  <tr>
+    <td>EEE</td>
+    <td>DLD</td>
+    <td>3.34</td>
+  </tr>
+</table>
+</body>
+</html
