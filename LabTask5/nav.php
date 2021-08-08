1381dfad950fd3a5fedba36948cd3c899f3b4bcb
Israt-28 <62091358+Israt-28@users.noreply.github.com>
2021-08-08 22:37:05 +0600

    Delete LabTask5 directory

--- LabTask5/nav.php
+++
@@ -1,5 +0,0 @@
- <ul>
-        <li><a href="/webtech/Lab5/addProduct.php"> Adding Product</a></li>
-        <li><a href="/webtech/Lab5/showAllProducts.php"> Showing all Product</a></li>
-        <li><a href="/webtech/Lab5/searchProduct.php"> Searching Product</a></li>
-</ul>
